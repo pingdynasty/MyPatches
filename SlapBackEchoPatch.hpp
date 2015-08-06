46122357b715cfc29eb79a1c99135811645723f8
mars <martin@rebel-it.com>
2015-08-06 16:21:13 +0100

    extended echo to up to 340ms

--- SlapBackEchoPatch.hpp
+++ SlapBackEchoPatch.hpp
@@ -7,7 +7,7 @@
 /**
  * Short slap back echo with stereo output separation.
  * 1 input, 2 output channels.
- * Adjust delay time from 17 to 170ms.
+ * Delay time is adjustable from 17 to 340 milliseconds.
  * Set Mix to 0 for dry signal in the left output and echo in the right.
  * Set Mix to 50% for dry and wet (echo) in both channels.
  * Set Mix to 100% for echo in the left channel and dry in the left.
@@ -15,32 +15,32 @@
 class SlapBackEchoPatch : public Patch {
 private:
   CircularBuffer* delayBuffer;
-  int delay;
+  float delay;
 public:
   SlapBackEchoPatch() : delay(0) {
     registerParameter(PARAMETER_A, "Delay");
     registerParameter(PARAMETER_D, "Mix");
-    delayBuffer = CircularBuffer::create(8*1024);  // 170ms
+    delayBuffer = CircularBuffer::create(16*1024);  // 170ms
   }
   ~SlapBackEchoPatch(){
     CircularBuffer::destroy(delayBuffer);
   }
   void processAudio(AudioBuffer &buffer){
-    float time = 0.1+0.9*getParameterValue(PARAMETER_A);
-    int newDelay = time*(delayBuffer->getSize()-1);
+    float time = 0.05+0.95*getParameterValue(PARAMETER_A);
+    float newDelay = time*(delayBuffer->getSize()-1);
+    int size = buffer.getSize();
+    float incr = (newDelay - delay)/size;
     float mix = getParameterValue(PARAMETER_D);
     float nmix = 1.0-mix;
     float* left = buffer.getSamples(0);
     float* right = buffer.getSamples(1);
-    int size = buffer.getSize();
     for(int n = 0; n < size; n++){
       float sample = left[n];
-      float x0 = (size-n)/(float)size;
-      float x1 = n/(float)size;
       delayBuffer->write(sample);
-      float echo = delayBuffer->read(delay)*x0 + delayBuffer->read(newDelay)*x1;
+      float echo = delayBuffer->interpolate(delay);
       left[n] = nmix*sample + mix*echo;
       right[n] = mix*sample + nmix*echo;
+      delay += incr;
     }
     delay = newDelay;
   }
