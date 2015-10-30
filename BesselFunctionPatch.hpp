4706905847e35b4440b3260b4ed0942c6d667915
mars <martin@rebel-it.com>
2015-10-30 09:03:20 +0000

    bessel oscillator

---
+++ BesselFunctionPatch.hpp
@@ -0,0 +1,47 @@
+#ifndef __BesselFunctionPatch_hpp__
+#define __BesselFunctionPatch_hpp__
+
+#include "StompBox.h"
+
+/*
+ * Bessel function of the first kind.
+ * Outputs Bessel function J of order 1 on left channel
+ * http://mathworld.wolfram.com/BesselFunctionoftheFirstKind.html
+ * http://www.boost.org/doc/libs/1_38_0/libs/math/doc/sf_and_dist/html/math_toolkit/special/bessel/bessel.html
+ */
+class BesselFunctionPatch : public Patch {
+private:
+  float x;
+  float y0;
+public:
+  BesselFunctionPatch(){
+    registerParameter(PARAMETER_A, "Rate");
+    registerParameter(PARAMETER_B, "Range");
+    registerParameter(PARAMETER_D, "Gain");
+    x = 0;
+    y0 = 0;
+  }
+  void processAudio(AudioBuffer &buffer){
+    float rate = (getParameterValue(PARAMETER_A))*0.8;
+    float range = getParameterValue(PARAMETER_B)*10000+10;
+    float gain = getParameterValue(PARAMETER_D)*10;
+    if(isButtonPressed(RED_BUTTON))
+      rate /= 10;
+    FloatArray left = buffer.getSamples(LEFT_CHANNEL);
+    FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
+    int size = buffer.getSize();
+    for(int i=0; i<size; i+=4){
+      float f = gain*j1f(x);
+      left[i] = f*0.25 + y0*0.75;
+      left[i+1] = f*0.5 + y0*0.5;
+      left[i+2] = f*0.75 + y0*0.25;
+      left[i+3] = f;
+      y0 = f;
+      x += rate;
+      if(x > range && abs(f) < 0.01)
+	x *= -1;
+    }
+  }
+};
+
+#endif   // __BesselFunctionPatch_hpp__
