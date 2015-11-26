0496a2fac73d09bc0e42a891d7e4706cae7b489b
mars <martin@rebel-it.com>
2015-11-26 17:42:28 +0000

    updated polyblep

--- PolyBlepPatch.hpp
+++ PolyBlepPatch.hpp
@@ -2,49 +2,78 @@
 #define __PolyBlepPatch_hpp__
 
 #include "StompBox.h"
+#include "Envelope.hpp"
 #include "oscillator.h"
 using namespace stmlib;
 
 class PolyBlepPatch : public Patch {
 private:
-  const float div;
   const float VOLTAGE_MULTIPLIER = -4.40f;
   // const float VOLTAGE_OFFSET = 0.028f;
   const float VOLTAGE_OFFSET = -0.0585f;
+  float mul;
   float sample2volts(float s){
     return (s-VOLTAGE_OFFSET) * VOLTAGE_MULTIPLIER;
   }
   float volts2hz(float v){
     return 440.f * powf(2, v);
   }
-  Oscillator osc;
+  AdsrEnvelope env;
+  FloatArray envelope;
+  Oscillator losc, rosc;
 public:
-  PolyBlepPatch(): div(getSampleRate()*64) {
+  PolyBlepPatch() : mul(1.0/getSampleRate()), env(getSampleRate()) {
     registerParameter(PARAMETER_A, "Tune");
     registerParameter(PARAMETER_B, "Shape");
-    registerParameter(PARAMETER_C, "PW");
+    registerParameter(PARAMETER_C, "Envelope");
     registerParameter(PARAMETER_D, "Gain");
-    osc.Init();
+    losc.Init();
+    rosc.Init();
+    env.setSustain(1.0);
+    env.setDecay(0.0);
+    env.setRelease(0.0);
+    envelope = FloatArray::create(getBlockSize());
+  }
+  ~PolyBlepPatch(){
+    FloatArray::destroy(envelope);
   }
   void processAudio(AudioBuffer &buffer) {
     float tune = getParameterValue(PARAMETER_A)*2.0 - 1.0;
-    float shape = getParameterValue(PARAMETER_B);
-    float pw = getParameterValue(PARAMETER_C)*0.8 + 0.1; // polyblep doesn't like full-range pwm
+    float shape = getParameterValue(PARAMETER_B)*2;
+    float pw = 0.5;
+    if(shape > 1.0){
+      pw += 0.49*(shape-1.0); // pw 0.5 to 0.99
+      shape = 1.0; // square wave
+    }
+    float c = getParameterValue(PARAMETER_C);
+    if(c < 0.33){
+      env.setAttack(c*3.33*2);
+      env.setRelease(0.0);
+    }else if(c < 0.66){
+      env.setAttack((0.66-c)*3.33*2);
+      env.setRelease((c-0.33)*3.33*2);
+    }else{
+      env.setAttack((c-0.66)*3.33*2);
+      env.setRelease(2.0);
+    }
+    env.gate(isButtonPressed(PUSHBUTTON), getSamplesSinceButtonPressed(PUSHBUTTON));
     float gain = getParameterValue(PARAMETER_D)*2;
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
     FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
     int size = buffer.getSize();
-    float freq = (tune * 100 + 100.00f) / getSampleRate();
-    // float frequency = 4*volts2hz(sample2volts(left[0]) + tune) / getSampleRate();
-    float frequency = (tune * 4000 + 4020)/getSampleRate();
-    osc.Render<true>(frequency, pw, shape, left, size);
-    // osc.Render<true>(tune, // master
-    // 		     tune * detune,  // slave
-    // 		     pw,  // pulse-width
-    // 		     shape,  // shape: 0.0 for saw, 1.0 for square
-    // 		     left, size);
-    left.multiply(gain);
-    right.copyFrom(left);
+    float lfreq = volts2hz(sample2volts(left[0]) + tune) * mul;
+    float rfreq = volts2hz(sample2volts(right[0]) + tune) * mul;
+    losc.Render<true>(lfreq, pw, shape, left, size);
+    rosc.Render<true>(rfreq, pw, shape, right, size);
+    env.getEnvelope(envelope);
+    if(gain < 1.0){
+      envelope.multiply(gain);
+    }else{
+      envelope.add(gain-1.0);
+      //      envelope.multiply(1.0/gain); // scale down to avoid clipping
+    }
+    left.multiply(envelope);
+    right.multiply(envelope);
   }
 };
 
