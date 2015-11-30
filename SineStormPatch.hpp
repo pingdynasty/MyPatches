b555523d991ce42ffe81d9a86823edd3f290ec4c
mars <martin@rebel-it.com>
2015-11-30 11:53:43 +0000

    refactored and added portamento

--- SineStormPatch.hpp
+++ SineStormPatch.hpp
@@ -1,35 +1,38 @@
 #include "StompBox.h"
 
+#include "Envelope.h"
+#include "VoltsPerOctave.h"
+#include "SmoothFloat.hpp"
+
 /*
+ * Oscillator that generates a cluster of sine waves
  */
 class SineStormPatch : public Patch {
 private:
   const static int DDS_LENGTH = 1<<14;
   const static int TONES = 18;
   float acc[TONES];
-  float inc[TONES];
-  float gain[TONES];
+  SmoothFloat inc[TONES];
+  SmoothFloat gain[TONES];
   FloatArray sine;
-  const float VOLTAGE_MULTIPLIER = -4.40f;
-  const float VOLTAGE_OFFSET = -0.0585f;
+  VoltsPerOctave hz;
   const float mul;
-  float sample2volts(float s){
-    return (s-VOLTAGE_OFFSET) * VOLTAGE_MULTIPLIER;
-  }
-  float volts2hz(float v){
-    return 440.f * powf(2, v);
-  }
 public:
-  SineStormPatch():mul(1.0/getSampleRate()){
+  SineStormPatch() :
+    mul(1.0/getSampleRate()){
     registerParameter(PARAMETER_A, "Pitch");
     registerParameter(PARAMETER_B, "Tones");
     registerParameter(PARAMETER_C, "Spread");
-    registerParameter(PARAMETER_D, "Gain");
+    registerParameter(PARAMETER_D, "Amplitude");
+    registerParameter(PARAMETER_E, "Portamento");
     sine = FloatArray::create(DDS_LENGTH);
     for(int i=0; i<DDS_LENGTH; ++i)
       sine[i] = sin(2*M_PI*i/(DDS_LENGTH-1));
-    for(int i=0; i<TONES; i++)
+    for(int i=0; i<TONES; i++){
       acc[i] = 0.0;
+      gain[i] = 0.0;
+      gain[i].lambda = 0.8;
+    }
   }
   ~SineStormPatch(){
     FloatArray::destroy(sine);
@@ -40,25 +43,27 @@ public:
     return sine[index];
   }
   void processAudio(AudioBuffer& buf){
-    float freq = getParameterValue(PARAMETER_A)*6.0 - 2.0;
-    float fraction = getParameterValue(PARAMETER_B)*(TONES-1)+1;
-    float spread = getParameterValue(PARAMETER_C) + 1.0;
-    float amp = getParameterValue(PARAMETER_D)*2.0 / fraction;
+    float freq = getParameterValue(PARAMETER_A)*8.0-4.0;
+    float fraction = getParameterValue(PARAMETER_B)*(TONES-1)+1.0;
+    float spread = getParameterValue(PARAMETER_C)*2.0+1.0;
+    float amp = getParameterValue(PARAMETER_D)*2.0;
+    float portamento = getParameterValue(PARAMETER_E)*0.19+0.8;
     int tones = (int)fraction;
-    fraction -= tones;
     FloatArray left = buf.getSamples(LEFT_CHANNEL);
-    FloatArray right = buf.getSamples(LEFT_CHANNEL);
-    inc[0] = volts2hz(sample2volts(left[0]) + freq) * mul;
-    amp += right[0]-VOLTAGE_OFFSET;
-    gain[0] = amp;
+    FloatArray right = buf.getSamples(RIGHT_CHANNEL);
+    hz.setTune(freq);
+    inc[0].lambda = portamento;
+    inc[0] = hz.getFrequency(left[0])*mul;
+    gain[0] = amp/tones;
     for(int t=1; t<tones; ++t){
+      inc[t].lambda = portamento;
       inc[t] = inc[t-1]*spread;
       if(inc[t] >= 0.4)
 	gain[t] = 0.0f;
       else if(t == tones-1)
-	gain[t] = fraction*amp;
+	gain[t] = ((float)fraction-tones)*gain[0];
       else
-	gain[t] = amp;
+	gain[t] = gain[0];
     }
     for(int i=0; i<buf.getSize(); ++i){
       left[i] = 0;
@@ -69,5 +74,6 @@ public:
 	  acc[t] -= 1.0;
       }
     }
+    right.copyFrom(left);
   }
 };
