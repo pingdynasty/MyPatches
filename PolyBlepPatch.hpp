76ee96ebada37522cd6a01152372dc671c463a0b
mars <martin@rebel-it.com>
2016-02-29 10:37:04 +0000

    updated to use library oscillator

--- PolyBlepPatch.hpp
+++ PolyBlepPatch.hpp
@@ -4,24 +4,20 @@
 #include "StompBox.h"
 #include "Envelope.h"
 #include "VoltsPerOctave.h"
-#include "oscillator.h"
-using namespace stmlib;
+#include "PolyBlepOscillator.h"
 
 class PolyBlepPatch : public Patch {
 private:
-  float mul;
   AdsrEnvelope env;
   FloatArray envelope;
-  Oscillator losc, rosc;
+  PolyBlepOscillator losc, rosc;
   VoltsPerOctave hz;
 public:
-  PolyBlepPatch() : mul(1.0/getSampleRate()), env(getSampleRate()) {
+  PolyBlepPatch() : losc(getSampleRate()), rosc(getSampleRate()), env(getSampleRate()) {
     registerParameter(PARAMETER_A, "Tune");
     registerParameter(PARAMETER_B, "Shape");
     registerParameter(PARAMETER_C, "Envelope");
     registerParameter(PARAMETER_D, "Gain");
-    losc.Init();
-    rosc.Init();
     env.setSustain(1.0);
     env.setDecay(0.0);
     env.setRelease(0.0);
@@ -31,7 +27,7 @@ public:
     FloatArray::destroy(envelope);
   }
   void processAudio(AudioBuffer &buffer) {
-    float tune = getParameterValue(PARAMETER_A)*10.0 - 5.0;
+    float tune = getParameterValue(PARAMETER_A)*10.0 - 6.0;
     float shape = getParameterValue(PARAMETER_B)*2;
     float pw = 0.5;
     if(shape > 1.0){
@@ -51,11 +47,17 @@ public:
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
     FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
     hz.setTune(tune);
-    float lfreq = hz.getFrequency(left[0]) * mul;
-    float rfreq = hz.getFrequency(right[0]) * mul;
+    float lfreq = hz.getFrequency(left[0]);
+    float rfreq = hz.getFrequency(right[0]);
     int size = buffer.getSize();
-    losc.Render<true>(lfreq, pw, shape, left, size);
-    rosc.Render<true>(rfreq, pw, shape, right, size);
+    losc.setFrequency(lfreq);
+    losc.setShape(shape);
+    losc.setPulseWidth(pw);
+    rosc.setFrequency(rfreq);
+    rosc.setShape(shape);
+    rosc.setPulseWidth(pw);
+    losc.getSamples(left);
+    rosc.getSamples(right);
     env.getEnvelope(envelope);
     if(gain < 1.0){
       envelope.multiply(gain);
