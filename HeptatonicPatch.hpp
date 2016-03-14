7af887d0c3bc578d2a69d282ba93a008fe12bd28
mars <martin@rebel-it.com>
2016-03-14 10:21:55 +0000

    updated includes

--- HeptatonicPatch.hpp
+++ HeptatonicPatch.hpp
@@ -1,18 +1,37 @@
 #include "StompBox.h"
 #include "VoltsPerOctave.h"
-#include "SmoothFloat.hpp"
+#include "SmoothValue.h"
 
+#define P1      1.0
+#define m2      (P1+1.0/12)
+#define M2      (P1+2.0/12)
+#define m3      (P1+3.0/12)
+#define M3      (P1+4.0/12)
+#define P4      (P1+5.0/12)
+#define d5      (P1+6.0/12)
+#define A4      (P1+6.0/12)
+#define P5      (P1+7.0/12)
+#define m6      (P1+8.0/12)
+#define M6      (P1+9.0/12)
+#define m7      (P1+10.0/12)
+#define M7      (P1+11.0/12)
+#define P8      (P1+12.0/12)
 #define SEMI    (1.0/12)
 #define WHOLE   (2.0/12)
-#define MODES 14
-static const float HEPTATONIC_TONES[2][14] = {
+#define HEPTATONIC_MODES 14
+// WHOLE, WHOLE, SEMI, WHOLE, WHOLE, WHOLE, SEMI     // Major scale
+// WHOLE, SEMI, WHOLE, WHOLE, SEMI, WHOLE+SEMI, SEMI // Harmonic Minor scale
+static const float HEPTATONIC_TONES[2][HEPTATONIC_MODES] = {
   // heptatonic scales
-  {WHOLE, WHOLE, SEMI, WHOLE, WHOLE, WHOLE, SEMI, // Major scale
-   WHOLE, WHOLE, SEMI, WHOLE, WHOLE, WHOLE, SEMI,},
-  {WHOLE, SEMI, WHOLE, WHOLE, SEMI, WHOLE+SEMI, SEMI, // Harmonic Minor scale
-   WHOLE, SEMI, WHOLE, WHOLE, SEMI, WHOLE+SEMI, SEMI }
+  {M2, m3, P4, P5, m6, M7, P8, // Harmonic Minor scale
+   P1+M2, P1+m3, P1+P4, P1+P5, P1+m6, P1+M7, P1+P8},
+  {M2, M3, P4, P5, M6, M7, P8, // Major scale
+   P1+M2, P1+M3, P1+P4, P1+P5, P1+M6, P1+M7, P1+P8}
 };
 // The major and natural minor scales share the same set of notes, but start in a different place.
+static const unsigned int HEPTATONIC_SEQUENCE[8] = {
+  0, 2, 4, 6, 3, 5, 1, 7
+};
 
 class HeptatonicPatch : public Patch {
 private:
@@ -37,8 +56,7 @@ public:
       sine[i] = sin(2*M_PI*i/(DDS_LENGTH-1));
     for(int i=0; i<TONES; i++){
       acc[i] = 0.0;
-      gain[i] = 0.0;
-      gain[i].lambda = 0.8;
+      gain[i].lambda = 0.8+i*0.02;
     }
   }
   ~HeptatonicPatch(){
@@ -50,30 +68,31 @@ public:
     return sine[index];
   }
   void processAudio(AudioBuffer& buf){
-    float freq = getParameterValue(PARAMETER_A)*8.0-4.0;
-    float fraction = getParameterValue(PARAMETER_B)*(TONES-1)+1.0;
-    int mode = getParameterValue(PARAMETER_C)*MODES;
-    int scale = 1; // minor
+    float pitch = getParameterValue(PARAMETER_A)*8.0-4.0;
+    float fraction = getParameterValue(PARAMETER_B)*TONES+1.0;
+    int mode = getParameterValue(PARAMETER_C)*HEPTATONIC_MODES;
+    int scale = 0; // minor
     if(mode > 6){
-      scale = 0;
+      scale = 1;
       mode = 13-mode;
     }
-    float amp = getParameterValue(PARAMETER_D)*2.0;
+    float amp = getParameterValue(PARAMETER_D);
     float portamento = getParameterValue(PARAMETER_E)*0.19+0.8;
     int tones = (int)fraction;
     FloatArray left = buf.getSamples(LEFT_CHANNEL);
     FloatArray right = buf.getSamples(RIGHT_CHANNEL);
-    hz.setTune(freq);
+    hz.setTune(pitch);
     inc[0].lambda = portamento;
     float basetone = hz.getFrequency(left[0])*mul;
-    for(int i=0; i<mode; ++i)
-      basetone *= 1.0+HEPTATONIC_TONES[scale][i];    
+    if(mode > 0)
+      basetone *= HEPTATONIC_TONES[scale][mode-1];
     inc[0] = basetone;
-    gain[0] = amp/tones;
+    gain[0] = amp/tones + hz.sampleToVolts(right[0]);
+    debugMessage("mode/tones", mode, tones);
     for(int t=1; t<tones; ++t){
-      float tone = 1.0 + HEPTATONIC_TONES[scale][mode++];
+      float tone = HEPTATONIC_TONES[scale][HEPTATONIC_SEQUENCE[t]+mode];
       inc[t].lambda = portamento;
-      inc[t] = inc[t-1]*tone;
+      inc[t] = inc[0]*tone;
       if(inc[t] >= 0.4)
 	gain[t] = 0.0f;
       else if(t == tones-1)
