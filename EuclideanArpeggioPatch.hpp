3c78a4bb1d86231ff626b3e4252d5234b4f0a819
mars <martin@rebel-it.com>
2016-03-20 14:11:54 +0000

    euclidean arpeggio patch based on Stoicheia sequence

---
+++ EuclideanArpeggioPatch.hpp
@@ -0,0 +1,74 @@
+#ifndef __EuclideanArpeggioPatch_hpp__
+#define __EuclideanArpeggioPatch_hpp__
+
+#include "StompBox.h"
+#include "SmoothValue.h"
+#include "Envelope.h"
+#include "Sequence.h"
+#include "PolyBlepOscillator.h"
+
+class EuclideanArpeggioPatch : public Patch {
+private:
+  AdsrEnvelope env;
+  PolyBlepOscillator osc[3];
+  FloatArray envelope;
+  SmoothFloat freq;
+  SmoothFloat gain;
+  bool buttonstate;
+  Sequence<uint16_t> seq;
+  float frequency;
+public:
+  EuclideanArpeggioPatch() :
+    env(getSampleRate()),
+    freq(0.9, 440.0),
+    gain(0.9) {
+    frequency = 880;
+    registerParameter(PARAMETER_A, "Pitch");
+    registerParameter(PARAMETER_B, "Steps");
+    registerParameter(PARAMETER_C, "Fills");
+    registerParameter(PARAMETER_D, "Divs");
+    for(int i=0; i<3; ++i){
+      osc[i].setSampleRate(getSampleRate());
+      osc[i].setShape(0.5);
+      osc[i].setPulseWidth(0.5);
+      osc[i].setFrequency(frequency);
+    }
+    env.setSustain(1.0);
+    env.setDecay(0.1);
+    env.setRelease(0.1);
+    envelope = FloatArray::create(getBlockSize());
+    buttonstate = 0;
+  }
+  ~EuclideanArpeggioPatch(){
+    FloatArray::destroy(envelope);
+  }
+  void processAudio(AudioBuffer &buffer) {
+    frequency = getParameterValue(PARAMETER_A)*1650+110;
+    int steps = getParameterValue(PARAMETER_B)*16;
+    int fills = getParameterValue(PARAMETER_C)*steps+1;
+    int divs = getParameterValue(PARAMETER_D)*12+1;
+    seq.calculate(steps, fills);    
+    if(isButtonPressed(PUSHBUTTON) != buttonstate){
+      buttonstate = isButtonPressed(PUSHBUTTON);
+      if(buttonstate){ // leading edge
+	bool on = seq.next();
+	int pos = seq.getPosition()+1;
+	osc[0].setFrequency(frequency*pos/divs);
+	osc[1].setFrequency(frequency*divs/pos);
+	env.gate(on, getSamplesSinceButtonPressed(PUSHBUTTON));
+      }else{
+	env.gate(false);
+      }
+    }
+    FloatArray left = buffer.getSamples(LEFT_CHANNEL);
+    FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
+    int size = buffer.getSize();
+    osc[0].getSamples(left);
+    osc[1].getSamples(right);
+    env.getEnvelope(envelope);
+    left.multiply(envelope);
+    right.multiply(envelope);
+  }
+};
+
+#endif   // __EuclideanArpeggioPatch_hpp__
---
+++ Sequence.h
@@ -0,0 +1,65 @@
+#ifndef _SEQUENCE_H_
+#define _SEQUENCE_H_
+
+#include <inttypes.h>
+#include "bjorklund.h"
+
+#ifdef SERIAL_DEBUG
+#include "serial.h"
+#endif // SERIAL_DEBUG
+
+#define SEQUENCE_ALGORITHM_ARRAY_SIZE 10
+
+template<typename T>
+class Sequence {
+public:
+ Sequence() : pos(0), length(1) {}
+
+  void calculate(uint8_t steps, uint8_t fills){
+    Bjorklund<T, SEQUENCE_ALGORITHM_ARRAY_SIZE> algo;
+    T newbits;
+    newbits = algo.compute(steps, fills);
+    length = steps;
+    bits = newbits;
+  }
+
+#ifdef SERIAL_DEBUG
+  void print(){
+    for(int i=0; i<length; ++i)
+      serialWrite(next() ? 'x' : '-');
+//       serialWrite((bits & (1UL<<i)) ? 'x' : '-');
+    printNewline();
+  }
+#endif
+
+  void reset(){
+    pos = offset % length;
+  }
+
+  void rotate(int8_t steps){
+    pos = (length + pos + steps - offset) % length;
+    offset = steps;
+  }
+
+  bool next(){
+    if(pos >= length)
+      pos = 0;
+    return bits & (1UL << pos++);
+  }
+
+  int getPosition(){
+    return pos;
+  }
+
+  int getLength(){
+    return length;
+  }
+
+private:
+  T bits;
+  uint8_t length;
+  int8_t offset;
+  volatile uint8_t pos;
+};
+
+#endif /* _SEQUENCE_H_ */
