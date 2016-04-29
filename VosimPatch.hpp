b08525bba02356d2568a5b60102df4a1f859675d
mars <martin@rebel-it.com>
2016-04-29 18:36:46 +0100

    added second formant

--- VosimPatch.hpp
+++ VosimPatch.hpp
@@ -9,13 +9,13 @@
 class VosimOscillator : public Oscillator {
 private:
   const float fs;
-  SineOscillator* formant;
-  // PolyBlepOscillator saw;
+  SineOscillator* formant1;
+  SineOscillator* formant2;
   float phase;
   float incr;
 public:
-  VosimOscillator(float samplerate, SineOscillator* osc) :
-    fs(samplerate), formant(osc) {}
+  VosimOscillator(float samplerate, SineOscillator* osc2, SineOscillator* osc1) :
+    fs(samplerate), formant1(osc1) , formant2(osc2) {}
   void setFrequency(float freq){
     incr = freq/fs;
   }
@@ -26,39 +26,47 @@ public:
     phase -= incr;
     if(phase <= 0.0){
       phase += 1.0;
-      formant->reset();
+      formant1->reset();
+      formant2->reset();
     }
-    float s = formant->getNextSample();
-    return s*s*phase;
+    float s1 = formant1->getNextSample();
+    float s2 = formant2->getNextSample();
+    return (s1*s1 + s2*s2)*phase*0.5;
   }
 };
 
 class VosimPatch : public Patch {
-  SineOscillator* sine;
+  SineOscillator* sine1;
+  SineOscillator* sine2;
   VosimOscillator* osc;
   VoltsPerOctave hz;
 public:
   VosimPatch(){
-    sine = new SineOscillator(getSampleRate());
-    osc = new VosimOscillator(getSampleRate(), sine);
-    registerParameter(PARAMETER_A, "Tune");
+    sine1 = new SineOscillator(getSampleRate());
+    sine2 = new SineOscillator(getSampleRate());
+    osc = new VosimOscillator(getSampleRate(), sine1, sine2);
+    registerParameter(PARAMETER_A, "Fundamental");
     registerParameter(PARAMETER_B, "Formant");
+    registerParameter(PARAMETER_B, "Offset");
     registerParameter(PARAMETER_D, "Gain");
   }
   void processAudio(AudioBuffer &buffer) {
     float tune = getParameterValue(PARAMETER_A)*10.0 - 6.0;
-    float formant = getParameterValue(PARAMETER_B)*10.0 - 6.0;
+    float f1 = getParameterValue(PARAMETER_B)*8.0 - 4.0;
+    float f2 = getParameterValue(PARAMETER_C)+1.0;
     float gain = getParameterValue(PARAMETER_D)*2;
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
     FloatArray right = buffer.getSamples(LEFT_CHANNEL);
     hz.setTune(tune);
     float lfreq = hz.getFrequency(left[0]);
     osc->setFrequency(lfreq);
-    hz.setTune(formant);
+    hz.setTune(f1);
     float rfreq = hz.getFrequency(right[0]);
-    sine->setFrequency(rfreq);
+    sine1->setFrequency(rfreq);
+    sine2->setFrequency(rfreq*f2);
     osc->getSamples(left);
     left.multiply(gain);
+    right.copyFrom(left);
   }
 };
 
