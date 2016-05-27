b33af0ec5e90ed44eeb16691e83d4f0b7905b045
mars <martin@rebel-it.com>
2016-05-27 16:53:07 +0100

    cleaned up and made more playable

--- HarmonicOscillator.hpp
+++ HarmonicOscillator.hpp
@@ -27,41 +27,35 @@ private:
   int Nstate; // 16
   int  HeightConstant; // 30
 
-  double HALFd;
-  int HalfSize;
+  float HALFd;
+  float HalfSize;
 
-  const double EnergyConstant = 0.5d; // increasing constant moves waveform up
-  const double MAX_VALUE = 127.0d;
+  const float EnergyConstant = 0.5f; // increasing constant moves waveform up
+  const float MAX_VALUE = 127.0f;
 
-  int controlvalues[controls];
-  int energyControl;
+  // int controlvalues[controls];
+  // int energyControl;
 
-  double dt = 0.001 * 30;
-  double t = 1.0d;
+  float dt = 0.001 * 30;
+  float t = 1.0f;
 
-  double AverageEnerg;
+  float AverageEnerg;
 
-  double aryCn[controls];
-  double rawCn[controls];
-  double alfan[controls];
-  double nfact[controls];
-  double PsiArray[samples+1][controls];
+  float aryCn[controls];
+  float rawCn[controls];
+  float alfan[controls];
+  float nfact[controls];
+  float PsiArray[samples+1][controls];
 
 public:
   // HarmonicOscillator(int ssamples, int ccontrols){
   HarmonicOscillator(){
-    // samples = ssamples;
-    // controls = ccontrols;
 
     Nstate = controls;
     HeightConstant = controls;
-    // aryCn = new double[HeightConstant];
-    // rawCn = new double[HeightConstant];
-    // alfan = new double[HeightConstant];
-    // nfact = new double[HeightConstant];
 
     // increasing HALFd moves center of waveform to the right
-    HALFd = controls / 2.0d; // 5.0d, samples / 10
+    HALFd = controls / 2.0f; // 5.0f, samples / 10
     //     int HalfSize= samples / 10;
     // increasing HalfSize moves center of waveform to the right
     HalfSize = samples / controls; // 21, samples / 10
@@ -72,19 +66,18 @@ public:
     for(int j=0;j<HeightConstant;j++)
       alfan[j] = fan(j);
         
-    nfact[0] = 1.0d;
+    nfact[0] = 1.0f;
     for(int j=1;j<HeightConstant;j++)
-      nfact[j] = nfact[j-1]*(double)j;
+      nfact[j] = nfact[j-1]*(float)j;
 
-    // PsiArray = new double[samples+1][HeightConstant];
     initialisePSIArray();
   }
 
 
   void initialisePSIArray(){
-    double exp, x;
+    float exp, x;
     for(int lpoint=0; lpoint <= samples; ++lpoint){
-      x = ((double)lpoint) / HalfSize - HALFd;
+      x = ((float)lpoint) / HalfSize - HALFd;
       exp = expf(-x * x / 2.0);
       for(int jquant=0; jquant < Nstate; jquant++)
 	PsiArray[lpoint][jquant] = PSI_n_form(x, jquant, exp);                    
@@ -96,8 +89,8 @@ public:
   //
   //    Constant factors used in alfan[] filled above
   //    fan(i) = 1/ (  sqrt( n!  2^i sqrt( Pi ) )
-  double fan(int i){ 
-    double d=1;
+  float fan(int i){ 
+    float d=1;
     for(int j=1; j<=i; j++)
       d=d*j;
     return( 1/ ( sqrtf(d*sqrtf(M_PI)*powf(2.0,i+0.0) ) ) );
@@ -105,16 +98,16 @@ public:
 
   //
   //    Hermite Polynomial
-  double hermite(int n, double x){ 
-    double h2 = 1.0d;
-    double h1 = 2.0d * x;
-    double hn = 1.0d;
+  float hermite(int n, float x){ 
+    float h2 = 1.0f;
+    float h1 = 2.0f * x;
+    float hn = 1.0f;
     if(n==0) 
       return h2;
     if(n==1) 
       return h1;
     for(int i=2; i<=n; ++i){
-      hn = 2.0d * x * h1 - 2.0d * ((double)i - 1.0d) * h2;
+      hn = 2.0f * x * h1 - 2.0f * ((float)i - 1.0f) * h2;
       h2 = h1; 
       h1 = hn;
     }
@@ -123,17 +116,17 @@ public:
 
   //    n-th state of Harm.Osc. in terms of Hermite Polynomial
   //    and the constant alfan[n+1]
-  double PSI_n_form(double x, int n, double exp){
+  float PSI_n_form(float x, int n, float exp){
     return(alfan[n] * hermite(n, x) * exp);
   }
 
   //
   //    Constructing the Wavefunction for a given  time
   //    and the constant alfan[n+1]
-  double PSI(double t, double x, int xx){  
-    double Cn;
-    double Re = 0;
-    double Im = 0;
+  float PSI(float t, float x, int xx){  
+    float Cn;
+    float Re = 0;
+    float Im = 0;
     for(int i=0;i<Nstate;i++){
       Cn = aryCn[i];
       if(Cn>0.0){
@@ -159,52 +152,29 @@ public:
   // HalfSize controls the width of the waveform
   // less means narrower band, higher frequency sound
   int getWavelength(){
-    return (int)(HalfSize * 4.0d * MAX_VALUE / samples);
+    return (int)(HalfSize * 4.0f * MAX_VALUE / samples);
   }
 
   void setWavelength(int wavelength){
     // HalfSize = samples / controls
-    HalfSize = (int)((wavelength * samples) / (4.0d * MAX_VALUE));
+    HalfSize = (int)((wavelength * samples) / (4.0f * MAX_VALUE));
     if(HalfSize == 0)
       HalfSize = 1;
     initialisePSIArray();
   }
 
-  int getEnergy(){
-    return energyControl;
-  }
-
-  void setEnergy(int eenergyControl){
-    energyControl = eenergyControl;
-    setGlauberState(energyControl);
-  }
-
-  int getControls(){
-    return controls; // controlvalues.length;
-  }
-
-  int getControl(int index){
-    // assert index < controlvalues.length;
-    return controlvalues[index];
-  }
-
-  void setControl(int index, int value){
-    //         System.out.println("control "+index+": "+value);
-    // assert index < controlvalues.length;
-    // assert value <= MAX_VALUE;
-    // assert value >= 0;
-    controlvalues[index] = value;
-    updateControlValues();
+  void setControl(int index, float value){
+    rawCn[index] = value;
   }
 
   //  Amplitude controls start
 
   // Normalize amplitudes
   void normalizeAmplitudes(){
-    double arySum = 0.0001d;
+    float arySum = 0.0001f;
     for(int i=0;i<Nstate;i++)
       arySum += rawCn[i] * rawCn[i];
-    double aryFact  = 1.0d / sqrtf(arySum);
+    float aryFact  = 1.0f / sqrtf(arySum);
 
     for(int i=0;i<Nstate;i++){
       aryCn[i] = rawCn[i] * aryFact;
@@ -214,22 +184,12 @@ public:
     // Evaluate AverageEnerg
     AverageEnerg = EnergyConstant;
     for(int i=0; i<controls; i++)
-      AverageEnerg += ((double)i) * aryCn[i] * aryCn[i];
-    energyControl = (int)((MAX_VALUE / 10.0d) * (AverageEnerg - EnergyConstant));
-  }
-
-  // Setting of amplitudes by scrollers
-  void updateControlValues(){
-    // for(int i=0;i<controlvalues.length;++i)
-    for(int i=0;i<controls;++i)
-      rawCn[i] = ((double)controlvalues[i]) / MAX_VALUE;
-
-    normalizeAmplitudes();
+      AverageEnerg += ((float)i) * aryCn[i] * aryCn[i];
+    // energyControl = (int)((MAX_VALUE / 10.0f) * (AverageEnerg - EnergyConstant));
   }
 
   // Setting of amplitudes for a single state
   void setSingleState(int Nvalue){
-    // assert Nvalue < controlvalues.length;
     //         System.out.println("single state "+Nvalue);
     for(int i=0; i<controls; ++i)
       rawCn[i] = 0.0;
@@ -239,27 +199,24 @@ public:
 
     for(int i=0; i<controls; ++i){
       rawCn[i] = MAX_VALUE * aryCn[i];
-      controlvalues[i] = (int)rawCn[i];
+      // controlvalues[i] = (int)rawCn[i];
     }
 
   }
 
   //    Setting of amplitudes for Glauber State by energy selector scroller
   void setGlauberState(int Nvalue){
-    //         System.out.println("glauber state "+Nvalue);
-    energyControl = Nvalue;
+    //         AverageEnerg=EnergyConstant+0.0001+(10.0-0.1*(float)Nvalue );  // SETENERGY
+    AverageEnerg = EnergyConstant + 10.0f * Nvalue / MAX_VALUE;  // SETENERGY
 
-    //         AverageEnerg=EnergyConstant+0.0001+(10.0-0.1*(double)Nvalue );  // SETENERGY
-    AverageEnerg = EnergyConstant + 10.0d * Nvalue / MAX_VALUE;  // SETENERGY
-
-    double Xx = sqrtf(AverageEnerg-EnergyConstant);
-    double Xx2;
+    float Xx = sqrtf(AverageEnerg-EnergyConstant);
+    float Xx2;
 
     for(int i=0; i<Nstate; ++i){
       //  Matlab values
       //  for N=2:10,fac(N)=fac(N-1)*(N-1);end
       //  for N=0:9,coef(N+1)=x^N/sqrt(fac(N+1))*exp(-0.5*x^2);end
-      Xx2 = 0.5d * powf(Xx, 2);
+      Xx2 = 0.5f * powf(Xx, 2);
       aryCn[i] = powf(Xx,i) / sqrtf(nfact[i]) * expf(-Xx2);
     }
 
@@ -268,67 +225,42 @@ public:
 
     for(int i=0; i<controls; ++i){
       rawCn[i] = MAX_VALUE * aryCn[i];
-      controlvalues[i] = (int)rawCn[i];
+      // controlvalues[i] = (int)rawCn[i];
     }
 
   }
 
   void calculate(float* values, int size){
-  // double* calculate() {  
     // calculating the wavefunction in sample points
-    // values = new double[samples];
-    // memset(values, 0, sizeof(values));
-    // calculate first value
     values[0] = PSI(t,0,0);
     for(int i=1; i<size; ++i)
       values[i] = PSI(t, i / HalfSize - HALFd, i);
-    // return values;
   }
 
-  // double* calculateNormalized() {  
-  //   // values = new double[samples];
-  //   memset(values, 0, sizeof(values));
-  //   values[0] = PSI(t,0,0);
-  //   double max = values[0];
-  //   double min = values[0];
-  //   for(int i=1; i<samples; ++i){
-  //     values[i] = PSI(t, i / HalfSize - HALFd, i);
-  //     if(values[i] > max)
-  // 	max = values[i];
-  //     if(values[i] < min)
-  // 	min = values[i];
-  //   }
-  //   for(int i=0; i<values.length; ++i)
-  //     values[i] = (values[i] - min) / (max - min);
-  //   return values;
-  // }
+  void calculateNormalized(float* values, int size){
+    values[0] = PSI(t,0,0);
+    float max = values[0];
+    float min = values[0];
+    for(int i=1; i<size; ++i){
+      values[i] = PSI(t, i / HalfSize - HALFd, i);
+      if(values[i] > max)
+  	max = values[i];
+      if(values[i] < min)
+  	min = values[i];
+    }
+    for(int i=0; i<size; ++i)
+      values[i] = (values[i] - min) / (max - min);
+  }
 
   void increment(){
     t += dt;
   }
 
-  // void dump(){
-  //   for(int j=0;j<HeightConstant;j++)
-  //     System.out.println("alfan["+j+"] "+alfan[j]);
-  //   for(int j=0;j<HeightConstant;j++)
-  //     System.out.println("nfact["+j+"] "+nfact[j]);
-
-  //   for(int lpoint=0; lpoint < samples; ++lpoint)
-  //     for(int jquant=0; jquant < Nstate; jquant++)
-  // 	System.out.println("PsiArray["+lpoint+"]["+jquant+"]"+PsiArray[lpoint][jquant]);
-  // }
-
-  void setTimeStep(int value){
-    dt = value * 0.001d;
+  void setTimeStep(float value){
+    dt = value * 0.1;
   }
 
-  int getTimeStep(){
-    return (int)(dt / 0.001d);
-  }
-
-  // Set the time delta value. Should be small, 0.001 - 0.100.
-  void setTimeStep(double ddt){
-    //         System.out.println("dt: "+dt);
-    dt = ddt;
+  float getTimeStep(){
+    return dt *10;
   }
 };
--- HarmonicOscillatorPatch.hpp
+++ HarmonicOscillatorPatch.hpp
@@ -2,42 +2,53 @@
 #define __HarmonicOscillatorPatch_hpp__
 
 #include "StompBox.h"
+#include "VoltsPerOctave.h"
 #include "HarmonicOscillator.hpp"
 #include "WavetableOscillator.h"
+#include "BiquadFilter.h"
 
 class HarmonicOscillatorPatch : public Patch {
 private:
-  static const int samples = 512;
+  static const int samples = 1024;
   static const int controls = 4;
   HarmonicOscillator<samples, controls> harms;
   WavetableOscillator* osc;
   FloatArray wavetable;
-  int count;
+  VoltsPerOctave hz;
 public:
 HarmonicOscillatorPatch(){
     wavetable = FloatArray::create(samples);
     osc = new WavetableOscillator(getSampleRate(), wavetable);
     osc->setFrequency(440);
+    // harms.setWavelength(64);
     for(int i=0; i<controls; ++i)
-      harms.setControl(i, 0.5*127);
-    harms.calculate(wavetable, samples);    
+      harms.setControl(i, 0.25);
+    harms.calculate(wavetable, samples);
   }
   void processAudio(AudioBuffer &buffer) {
-    float a = getParameterValue(PARAMETER_A);
+    float tune = getParameterValue(PARAMETER_A)*8.0 - 6.0;
+    // float a = getParameterValue(PARAMETER_A);
     float b = getParameterValue(PARAMETER_B);
     float c = getParameterValue(PARAMETER_C);
     float d = getParameterValue(PARAMETER_D);
-    // if(++count > a*5000){
-    //   count = 0;
-      harms.setControl(0, a*127);
-      harms.setControl(1, b*127);
-      harms.setControl(2, c*127);
-      harms.setControl(3, d*127);
-      harms.calculate(wavetable, samples);
-    // }
+    float e = getParameterValue(PARAMETER_E);
+    harms.setTimeStep(b);
+    // harms.setControl(0, b);
+    harms.setControl(1, c);
+    harms.setControl(2, d);
+    harms.setControl(3, e);
+    harms.normalizeAmplitudes();
+    // harms.calculate(wavetable, samples);
+    harms.calculateNormalized(wavetable, samples);
+    harms.increment();
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
-    osc->setFrequency(d*2000);
+    FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
+    hz.setTune(tune);
+    float freq = hz.getFrequency(left[0]);
+    osc->setFrequency(freq);
     osc->getSamples(left);
+    right.copyFrom(left);
+    right.multiply(0.1);
   }
 };
 
