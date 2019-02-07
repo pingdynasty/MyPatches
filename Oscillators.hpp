84419b41e014376498afa5cad930710d5263865b
Martin <mars@pingdynasty.com>
2019-02-07 16:38:43 +0000

    Sine and Chirp moved to library

--- Oscillators.hpp
+++ Oscillators.hpp
@@ -2,6 +2,8 @@
 #define __Oscillators_hpp__
 
 #include "Oscillator.h"
+#include "SineOscillator.h"
+#include "ChirpOscillator.h"
 
 class SquareOscillator : public Oscillator {
 private:
@@ -76,51 +78,6 @@ public:
   }
 };
 
-class SineOscillator : public Oscillator {
-private:
-  float mul;
-  float phase;
-  float incr;
-public:
-  SineOscillator() : phase(0.0f), incr(0.0f) {    
-    setSampleRate(48000);
-  }
-  SineOscillator(float sr) : phase(0.0f), incr(0.0f){
-    setSampleRate(sr);
-  }    
-  void setSampleRate(float sr){
-    mul = 2*M_PI/sr;
-  }
-  void setFrequency(float freq){
-    incr = freq*mul;
-  }
-  void setPhase(float ph){
-    phase = ph;
-    while(phase >= 2*M_PI)
-      phase -= 2*M_PI;
-  }
-  void reset(){
-    phase = 0.0f;
-  }
-  float getPhase(){
-    return phase;
-  }
-  float getNextSample(){
-    float sample = sinf(phase);
-    phase += incr;
-    if(phase >= 2*M_PI)
-      phase -= 2*M_PI;
-    return sample;
-  }
-  float getNextSample(float fm){
-    float sample = sinf(phase);
-    phase += incr + fm;
-    if(phase >= 2*M_PI)
-      phase -= 2*M_PI;
-    return sample;
-  }  
-};
-
 class ImpulseOscillator : public Oscillator {
 private:
   bool triggered = false;
@@ -133,42 +90,6 @@ public:
       return 0.0f;
     triggered = true;
     return 1.0f;
-  }
-};
-
-class ChirpOscillator : public Oscillator {
-private:
-  const float fs;
-  float phase;
-  float incr;
-public:
-  float rate;
-  ChirpOscillator(float sr)
-    : fs(sr), phase(0.0f), incr(1.0f){}
-  void setFrequency(float freq){
-    incr = freq*2*M_PI/fs;
-  }
-  void setRate(float r){
-    // should be: rate 0: 1 (constant), rate -1: 1-0.5/sr (halved in a second), rate 1: 1+1/sr (doubled in a second)
-    if(r < 0)
-      rate = 1.0f - 10*(1/(1-r))/fs;
-    else
-      rate = 1.0f + 10*(1/(1+r))/fs;
-  }
-  void setDecay(float d){
-    setRate(-(d+1/fs));
-  }
-  void trigger(){
-    phase = 0.0f;
-  }
-  float getNextSample(){
-    float sample = sinf(phase);
-    phase += incr;
-    incr *= rate;
-    // phase %= 2*M_PI;
-    // if(phase >= 2*M_PI)
-    //   phase -= 2*M_PI;
-    return sample;
   }
 };
 
