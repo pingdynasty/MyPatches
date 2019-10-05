924af7328c4423cda8afae7830c9b83501a51d0d
Martin <mars@pingdynasty.com>
2019-10-05 14:05:24 +0200

    updated to support any number of channels

--- EmgPatch.hpp
+++ EmgPatch.hpp
@@ -22,32 +22,59 @@ public:
   // void process(float alpha, FloatArray input, FloatArray output){
 };
 
+class EmgSignalProcessor {
+  const float sr;
+  BiquadFilter* hp;
+  BiquadFilter* notch;
+  // EmgEnvelope env;
+public:
+  EmgSignalProcessor(float sr): sr(sr){
+    hp = BiquadFilter::create(5);
+    notch = BiquadFilter::create(5);
+    // initial filter settings
+    hp->setHighPass(10.0f/(sr/2), FilterStage::BUTTERWORTH_Q);
+    notch->setNotch(50.0f/(sr/2), 8);
+  }
+  ~EmgSignalProcessor(){
+    BiquadFilter::destroy(hp);
+    BiquadFilter::destroy(notch);
+  }
+  void processRaw(FloatArray samples, float gain, float hpfc, float notchq){
+    hp->setHighPass(hpfc/(sr/2), FilterStage::BUTTERWORTH_Q);
+    notch->setNotch(50.0f/(sr/2), notchq);
+    hp->process(samples);
+    notch->process(samples);
+    samples.multiply(gain);
+    samples.clip();
+  }
+  // void processEnv(FloatArray samples, float alpha){
+  //   env.process(alpha, left.getData(), right.getData(), left.getSize());    
+  // }
+};
+
 class EmgPatch : public Patch {
 private:
-  StereoBiquadFilter* hp;
-  StereoBiquadFilter* notch;
-  float sr;
-  EmgEnvelope env;
+  EmgSignalProcessor** processor;
 public:
   EmgPatch() {
-    hp = StereoBiquadFilter::create(5);
-    notch = StereoBiquadFilter::create(5);
-    // initial filter settings
-    hp->setHighPass(10.0f/(getSampleRate()/2), FilterStage::BUTTERWORTH_Q);
-    notch->setNotch(50.0f/(getSampleRate()/2), FilterStage::BUTTERWORTH_Q);
+    processor = new EmgSignalProcessor*[4];
+    for(int ch=0; ch<getNumberOfChannels(); ++ch)
+      processor[ch] = new EmgSignalProcessor(getSampleRate());
     registerParameter(PARAMETER_A, "Gain");
     registerParameter(PARAMETER_B, "HP Fc");
     registerParameter(PARAMETER_C, "Notch Q");
     registerParameter(PARAMETER_D, "Env Alpha");
     // initialise parameter settings
-    setParameterValue(PARAMETER_A, 0);
-    setParameterValue(PARAMETER_B, 0);
-    setParameterValue(PARAMETER_C, 0);
+    setParameterValue(PARAMETER_A, 0.5);
+    setParameterValue(PARAMETER_B, 0.5);
+    setParameterValue(PARAMETER_C, 0.5);
     setParameterValue(PARAMETER_D, 0);
   }
   ~EmgPatch() {
-    StereoBiquadFilter::destroy(hp);
-    StereoBiquadFilter::destroy(notch);
+    for(int ch=0; ch<getNumberOfChannels(); ++ch){
+      delete processor[ch];
+    }
+    delete[] processor;
   }
 
   void processAudio(AudioBuffer &buffer) {
@@ -55,23 +82,17 @@ public:
     float fc = getParameterValue(PARAMETER_B)*10+4;
     float q = getParameterValue(PARAMETER_C)*20+0.707;
     float alpha = getParameterValue(PARAMETER_D)*0.7+0.299;
-    FloatArray left = buffer.getSamples(LEFT_CHANNEL);
-    FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
-    hp->setHighPass(fc/(getSampleRate()/2), FilterStage::BUTTERWORTH_Q);
-    notch->setNotch(50.0f/(getSampleRate()/2), q);
-    hp->process(buffer);
-    notch->process(buffer);
-    left.multiply(gain);
-    right.multiply(gain);
-    env.process(alpha, left.getData(), right.getData(), left.getSize());
-    debugMessage("gain/range/rms", gain, abs(left.getMaxValue()-left.getMinValue()), left.getRms());
-    left.clip();
-    right.clip();
-    MidiMessage msg;
-    msg = MidiMessage::pb(1, left.getMean()*8192); // Pitch Bend filtered EMG 
-    sendMidi(msg);
-    msg = MidiMessage::cc(1, 1, right.getMean()*128); // Control Change envelope
-    sendMidi(msg);
+    for(int ch=0; ch<buffer.getChannels(); ++ch){
+      FloatArray samples = buffer.getSamples(ch);
+      processor[ch]->processRaw(samples, gain, fc, q);
+      MidiMessage msg;
+      msg = MidiMessage::pb(1+ch, samples.getMean()*8192); // Pitch Bend filtered EMG 
+      sendMidi(msg);
+      // processor[ch]->processEnv(samples);
+      // msg = MidiMessage::cc(1, 1, samples.getMean()*128); // Control Change envelope
+      // sendMidi(msg);
+    }
+    debugMessage("sr/bs/ch", (int)getSampleRate(), getBlockSize(), buffer.getChannels());
   }
 };
 
