a8974e5c4388566f2075de8a60e0b52e394641b5
Martin Klang <martin@rebel-it.com>
2020-10-10 15:19:43 +0200

    added tempo synced LFO output on F and G

--- TempoSyncedPingPongDelayPatch.hpp
+++ TempoSyncedPingPongDelayPatch.hpp
@@ -5,6 +5,8 @@
 #include "BiquadFilter.h"
 #include "CircularBuffer.hpp"
 #include "TapTempo.hpp"
+#include "SineOscillator.h"
+#include "SmoothValue.h"
 
 static const int RATIOS_COUNT = 9;
 static const float ratios[RATIOS_COUNT] = { 1.0/4, 
@@ -16,11 +18,6 @@ static const float ratios[RATIOS_COUNT] = { 1.0/4,
 					    2.0,
 					    3.0, 
 					    4.0 };
-#define P_A PARAMETER_A
-#define P_B PARAMETER_B
-#define P_C PARAMETER_C
-#define P_D PARAMETER_D
-#define P_E PARAMETER_E
 
 class TempoSyncedPingPongDelayPatch : public Patch {
 private:
@@ -31,47 +28,69 @@ private:
   TapTempo<TRIGGER_LIMIT> tempo;
   StereoBiquadFilter* highpass;
   StereoBiquadFilter* lowpass;
+  SineOscillator* lfo;
+  SmoothFloat lfoFreq;
 public:
   TempoSyncedPingPongDelayPatch() : 
     delayL(0), delayR(0), tempo(getSampleRate()*60/120) {
-    registerParameter(P_A, "Tempo");
-    registerParameter(P_B, "Feedback");
-    registerParameter(P_C, "Ratio");
-    registerParameter(P_D, "Dry/Wet");
-    registerParameter(P_E, "Drop");
+    registerParameter(PARAMETER_A, "Tempo");
+    registerParameter(PARAMETER_B, "Feedback");
+    registerParameter(PARAMETER_C, "Ratio");
+    registerParameter(PARAMETER_D, "Dry/Wet");
+    registerParameter(PARAMETER_E, "Drop");
+    registerParameter(PARAMETER_F, "LFO Sine>");
+    registerParameter(PARAMETER_G, "LFO Ramp>");
     delayBufferL = CircularBuffer::create(TRIGGER_LIMIT);
     delayBufferR = CircularBuffer::create(TRIGGER_LIMIT);
     highpass = StereoBiquadFilter::create(1);
     highpass->setHighPass(40/(getSampleRate()/2), FilterStage::BUTTERWORTH_Q); // dc filter
     lowpass = StereoBiquadFilter::create(1);
     lowpass->setLowPass(8000/(getSampleRate()/2), FilterStage::BUTTERWORTH_Q);
+    lfo = SineOscillator::create(getSampleRate()/getBlockSize());    
   }
+
   ~TempoSyncedPingPongDelayPatch(){
     CircularBuffer::destroy(delayBufferL);
     CircularBuffer::destroy(delayBufferR);
     StereoBiquadFilter::destroy(highpass);
     StereoBiquadFilter::destroy(lowpass);
+    SineOscillator::destroy((SineOscillator*)lfo);
   }
+
   float delayTime(int ratio){
     float time = tempo.getPeriod() * ratios[ratio];
     time = max(0.0001, min(0.9999, time));
     return time;
   }
+
+  void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples){
+    bool set = value != 0;
+    switch(bid){
+    case BUTTON_A:
+      tempo.trigger(set, samples);
+      break;
+    case BUTTON_B:
+      setParameterValue(PARAMETER_E, value);
+      if(set)
+      	lfo->reset();
+      break;
+    }
+  }
+  
   void processAudio(AudioBuffer& buffer){
-    int speed = getParameterValue(P_A)*4096;
-    float feedback = getParameterValue(P_B);
-    float drop = 1.0-getParameterValue(P_E);
+    int speed = getParameterValue(PARAMETER_A)*4096;
+    float feedback = getParameterValue(PARAMETER_B);
+    float drop = 1.0-getParameterValue(PARAMETER_E);
     if(drop < 0.01)
       feedback = 1.0;
-    int ratio = (int)(getParameterValue(P_C) * RATIOS_COUNT);
+    int ratio = (int)(getParameterValue(PARAMETER_C) * RATIOS_COUNT);
     int size = buffer.getSize();
-    tempo.trigger(isButtonPressed(PUSHBUTTON));
     tempo.clock(size);
     tempo.setSpeed(speed);
     float time = delayTime(ratio);
     int newDelayL = time*(delayBufferL->getSize()-1);
     int newDelayR = time*(delayBufferR->getSize()-1);
-    float wet = getParameterValue(P_D);
+    float wet = getParameterValue(PARAMETER_D);
     float dry = 1.0-wet;
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
     FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
@@ -94,6 +113,14 @@ public:
     }
     delayL = newDelayL;
     delayR = newDelayR;
+    // Tempo synced LFO
+    lfoFreq = getSampleRate()/(time*TRIGGER_LIMIT);
+    lfo->setFrequency(lfoFreq);
+    float value = lfo->getNextSample()*0.5+0.5;
+    setParameterValue(PARAMETER_F, value);
+    value = lfo->getPhase()/(2*M_PI);
+    setParameterValue(PARAMETER_G, value);
+    setButton(BUTTON_E, value < 0.5);
   }
 };
 
