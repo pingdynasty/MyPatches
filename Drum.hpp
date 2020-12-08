84b17af09ef61595854505535d1c8ea678323117
Martin Klang <martin@rebel-it.com>
2020-12-08 14:07:14 +0100

    include Envelope and Oscillator.h

--- Drum.hpp
+++ Drum.hpp
@@ -1,6 +1,9 @@
 #ifndef __Drum_hpp__
 #define __Drum_hpp__
 
+#include "Envelope.h"
+#include "Oscillator.h"
+
 class Drum : public Oscillator, public Envelope {
 public:
   virtual void setDecay(float amount){}
